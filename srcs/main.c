b7c1f0963c3b3fc3ed0b672d59a75ba877745aa3
Nicolas Salle <nsalle@z1r3p1.42lyon.fr>
2021-01-15 15:33:57 +0100

    usage

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,20 @@
 /*   By: nsalle <nsalle@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 13:31:32 by nsalle            #+#    #+#             */
-/*   Updated: 2021/01/15 12:05:10 by nsalle           ###   ########lyon.fr   */
+/*   Updated: 2021/01/15 15:33:28 by nsalle           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/computor.h"
 
-void	usage()
+void	usage(void)
+{
+	ft_putendl("Wrong usage of computor");
+	ft_putendl("Usage: ./computor \"equation\"");
+	exit (0);
+}
+
+void	check_degree(t_maths *master)
 {
 	
 }
@@ -21,11 +28,12 @@ int		main(int argc, char **argv)
 {
 	t_maths master;
 
-	master.left_exp = malloc(sizeof(t_expr));
-	master.right_exp = malloc(sizeof(t_expr));
 	if (argc != 2)
 		usage();
+	master.left_exp = malloc(sizeof(t_expr));
+	master.right_exp = malloc(sizeof(t_expr));
 	scanstr(argv[1]);
 	split_expressions(&master, argv[1]);
+	check_degree(&master);
 	return (0);
 }
