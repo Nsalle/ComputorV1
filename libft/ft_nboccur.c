4cf17f685429d478bb3721f1f1b1b1a354af549c
Nicolas Salle <nsalle@z1r3p1.42lyon.fr>
2021-01-15 12:38:57 +0100

    Norme

--- libft/ft_nboccur.c
+++ libft/ft_nboccur.c
@@ -6,24 +6,24 @@
 /*   By: nsalle <nsalle@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 13:53:01 by nsalle            #+#    #+#             */
-/*   Updated: 2021/01/14 13:53:36 by nsalle           ###   ########lyon.fr   */
+/*   Updated: 2021/01/15 12:38:43 by nsalle           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_nboccur(char *str, char c)
+int	ft_nboccur(char *str, char c)
 {
-    int i;
-    int count;
+	int i;
+	int count;
 
-    i = 0;
-    count = 0;
-    while (str[i])
-    {
-        if (str[i] == c)
-            count++;
-        i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
 }
