3596464e19c2afd32b24c966e8c597132d9db92b
Romain Codazzi <rcodazzi@z2r10p8.42lyon.fr>
2021-02-15 10:57:57 +0100

    second degree add

--- srcs/second_degree.c
+++ srcs/second_degree.c
@@ -1,16 +1,35 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   second_degree.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rcodazzi <rcodazzi@student.42lyon.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/15 15:14:09 by rcodazzi          #+#    #+#             */
-/*   Updated: 2021/01/15 15:37:24 by rcodazzi         ###   ########lyon.fr   */
-/*                                                                            */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   second_degree.c                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rcodazzi <rcodazzi@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2021/01/15 15:14:09 by rcodazzi     #+#   ##    ##    #+#       */
+/*   Updated: 2021/01/20 13:57:01 by rcodazzi    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
 /* ************************************************************************** */
-
 #include "../includes/computor.h"
+
+
+float absolute(float num)
+{
+    if(num < 0){
+        num = -num;
+    }
+    return num;
+}
+
+float ft_sqrt(float x)
+{
+    const float difference = 0.00001;
+    float guess = 1.0;
+    while(absolute(guess * guess - x) >= difference){
+        guess = (x/guess + guess)/2.0;
+    }
+    return guess;
+}
 
 void    solversecondegree(t_expr *m)
 {
@@ -18,9 +37,13 @@ void    solversecondegree(t_expr *m)
         4*m->degree2*m->degree0;
     if(m->discriminant == 0)
         ft_isnul(m);
-    if(m->discriminant > 0 )
+    else if(m->discriminant > 0 )
         ft_ispositive(m);
-    // ft_isnegative;
+    else if (m->discriminant < 0)
+    {
+        m->discriminant *= -1;
+        ft_isnegative(m);
+    }
 }
 
 
@@ -37,7 +60,19 @@ void ft_ispositive(t_expr *m)
     float res1;
     float res2;
 
-    res1 = (-m->degree1 - m->discriminant) / (2* m ->degree2);
+    res1 = (-m->degree1 - ft_sqrt(m->discriminant)) / (2* m->degree2);
+    res2 = (-m->degree1 + ft_sqrt(m->discriminant)) / (2* m->degree2);
 
     ft_printf("Discriminant is strictly positive, the two solutions are: \n%f\n%f" , res1,res2);
+}
+
+void ft_isnegative(t_expr *m)
+{
+    float delta;
+
+    delta = ft_sqrt(m->discriminant);
+
+    ft_printf("Discriminant is strictly positive, the two solutions are: \n ");
+    ft_printf("-%f - %fi \n-%f + %fi \n", m->degree1, m->degree2, m->degree1, m->degree2);
+    
 }
